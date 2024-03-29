ab7d6503b5d85d70f6b1ce3a8c95894196045bc7
d4gl4s <daglas.aitsen@gmail.com>
2024-03-29 18:25:56 +0200

    backend functionality for making purchases

--- backend/src/main/java/com/CGI/backend/repository/PurchaseRepository.java
+++ backend/src/main/java/com/CGI/backend/repository/PurchaseRepository.java
@@ -1,11 +1,13 @@
 package com.CGI.backend.repository;
 
+import com.CGI.backend.models.Movie;
 import com.CGI.backend.models.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
@@ -15,4 +17,6 @@ public interface PurchaseRepository extends JpaRepository<Purchase, Integer> {
     Set<Integer> findSeatNumbersByMovieId(@Param("movieId") Long movieId);
 
     List<Purchase> findByUserId(Long userId);
+
+    int countByMovieAndSeatNumbersIn(Movie movie, Set<Integer> seatNumbers);
 }
--- backend/src/main/java/com/CGI/backend/services/PurchaseService.java
+++ backend/src/main/java/com/CGI/backend/services/PurchaseService.java
@@ -1,9 +1,14 @@
 package com.CGI.backend.services;
 
+import com.CGI.backend.models.Movie;
 import com.CGI.backend.models.Purchase;
 import com.CGI.backend.repository.PurchaseRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
@@ -11,6 +16,24 @@ public class PurchaseService {
     private final PurchaseRepository purchaseRepository;
 
     public void addPurchase(Purchase purchase) {
+        // Check for null values
+        if (purchase.getUserId() == null || purchase.getSeatNumbers() == null || purchase.getMovie() == null)
+            throw new IllegalArgumentException("UserId, seatNumbers, and movie must be provided.");
+
+        // check if the seats provided are available,
+        boolean seatsAvailable = checkSeatAvailability(purchase);
+        if(!seatsAvailable) throw new IllegalArgumentException("The provided seats are not available");
+
+        // check if the movie provided start time is not in the past
+        LocalTime currentTime = LocalTime.now();
+        LocalTime startTime = purchase.getMovie().getStartTime();
+        if(startTime.isBefore(currentTime)) throw new IllegalArgumentException("The movie has already started");
+
         purchaseRepository.save(purchase);
+    }
+
+
+    private boolean checkSeatAvailability(Purchase purchase) {
+        return purchaseRepository.countByMovieAndSeatNumbersIn(purchase.getMovie(), purchase.getSeatNumbers()) == 0;
     }
 }
